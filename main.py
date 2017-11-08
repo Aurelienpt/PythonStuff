b81651cb72bc75130828e310e82e47a121c7e269
Aurelienpt <aurelpt@gmail.com>
2017-11-08 13:42:11 +0100

    1st commit bc i'm pro

---
+++ main.py
@@ -0,0 +1,9 @@
+text = input("Input : ")
+liste = list(text)
+newText = ""
+for x in range(0, len(liste)//2):
+	#TransLeBg - 50% sur le site sublime_text et aurele.oules.com
+	newText += liste[x]
+print(newText)
+
+# IZI PEASY LEMON SQUEEZY
