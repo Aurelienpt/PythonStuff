0ec30b00c53e8b61ed999bd5af34477a2083f30b
Aurelienpt <aurelpt@gmail.com>
2017-11-10 12:31:46 +0100

    Removed a commentary

--- HalfSentence.py
+++ HalfSentence.py
@@ -2,7 +2,6 @@ text = input("Input : ")
 liste = list(text)
 newText = ""
 for x in range(0, len(liste)//2):
-	#TransLeBg - 50% sur le site sublime_text et aurele.oules.com
 	newText += liste[x]
 print(newText)
 
